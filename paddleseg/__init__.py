bde772fb0539c800b93fb6477c3784929247d2f4
JoannaLin31 <87681534+JoannaLin31@users.noreply.github.com>
2024-09-03 16:46:28 +0800

    Update __init__.py

--- paddleseg/__init__.py
+++ paddleseg/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import models, datasets, transforms, optimizers
+from . import models, datasets, transforms, optimizers, utils
 
 __version__ = '0.0.0.dev0'
